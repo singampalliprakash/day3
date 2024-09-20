275f72c9ad9a884faeba0ba1f731c87bb68579a0
singampalliprakash <jnanaprakash2002@gmail.com>
2024-09-20 21:50:30 +0530

    Add files via upload

---
+++ Creating a List.py
@@ -0,0 +1,28 @@
+# Creating a List
+List = []
+print("Initial blank List: ")
+print(List)
+
+# Addition of Elements in the List
+List.append(1)
+List.append(2)
+List.append(4)
+print("\nList after Addition of Three elements: ")
+print(List)
+
+# Adding elements to the List using Iterator
+for i in range(1, 4):
+    List.append(i)
+print("\nList after Addition of elements from 1-3: ")
+print(List)
+
+# Adding Tuples to the List
+List.append((5, 6))
+print("\nList after Addition of a Tuple: ")
+print(List)
+
+# Addition of List to a List
+List2 = ['singampalli', 'prakash']
+List.append(List2)
+print("\nList after Addition of a List: ")
+print(List)
